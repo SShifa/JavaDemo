571c8338b5633596dfa559e421f90c0340a072f8
SShifa <sifasasture@gmail.com>
2022-12-28 18:46:01 +0530

    first commit

---
+++ src/com/Velocity/Test.java
@@ -0,0 +1,15 @@
+package com.Velocity;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		al.add(10);
+		al.add(20);
+		System.out.println(al);
+
+	}
+
+}
